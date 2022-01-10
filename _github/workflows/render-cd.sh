45131102e37dc6922e19a14b02f0a9411b0d6dc2
robertchoi80 <taeil.choi@sk.com>
2022-01-10 11:24:38 +0900

    Update render-cd.sh
    use git repo owner as username

--- _github/workflows/render-cd.sh
+++ _github/workflows/render-cd.sh
@@ -4,7 +4,6 @@ DECAPOD_BASE_URL=https://github.com/openinfradev/${DECAPOD_BASE_DIR}.git
 TKS_CUSTOM_BASE_DIR=tks-custom-base-yaml
 TKS_CUSTOM_BASE_URL=https://$USER_NAME:$API_TOKEN_GITHUB@github.com/openinfradev/${TKS_CUSTOM_BASE_DIR}.git
 BRANCH="main"
-USERNAME="TKS-BOT"
 
 rm -rf $DECAPOD_BASE_DIR $TKS_CUSTOM_BASE_DIR
 
